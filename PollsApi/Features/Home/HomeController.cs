2bcacf29d3f6fc255a13a162976645664b4a77e0
afdezayl <mekin777@gmail.com>
2020-07-16 18:25:43 +0200

    Agregar archivos de proyecto.

---
+++ PollsApi/Features/Home/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PollsApi.Features.Home
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Welcome()
+        {
+            return Ok("Welcome to the API!");
+        }
+    }
+}
